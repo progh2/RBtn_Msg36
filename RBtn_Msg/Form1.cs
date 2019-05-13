9e9a1228d78edd84eaef169a6f3f0859ba8f69c8
Gihun Ham <progh2@gmail.com>
2019-05-13 14:25:28 +0900

    #2 코드 구현 - 라디오 버튼 선택하고 해당하는 메시지박스 보여주기 구현

--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -11,9 +11,42 @@ namespace RBtn_Msg
 {
     public partial class Form1 : Form
     {
+        private MessageBoxButtons mbb;
+        private MessageBoxIcon mbi;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (this.rbOk.Checked)
+            {
+                this.mbb = MessageBoxButtons.OK;
+            }else if (this.rbOkCancel.Checked)
+            {
+                this.mbb = MessageBoxButtons.OKCancel;
+            }
+            else if (this.rbYesNo.Checked)
+            {
+                this.mbb = MessageBoxButtons.YesNo;
+            }
+
+            if (this.rbError.Checked)
+            {
+                this.mbi = MessageBoxIcon.Error;
+            }else if(this.rbInformation.Checked)
+            {
+                this.mbi = MessageBoxIcon.Information;
+            }
+            else if (this.rbQuestion.Checked)
+            {
+                this.mbi = MessageBoxIcon.Question;
+            }
+
+            MessageBox.Show("메시지 박스를 확인하세요~", "알림",
+                mbb, mbi);
         }
     }
 }
